3bb7c2c2326bcbd42f388a1943d8159fc5c80878
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-08 01:25:15 +0000

    Snapshot of branch master taken on 2019-04-07T21:25:14.034-04:00 from "https://github.com/UnofficialJuliaMirror/Poltergeist.jl-89210da2-f0be-5ef8-ba65-10393cce509d"

---
+++ src/Poltergeist.jl
@@ -0,0 +1,28 @@
+__precompile__()
+
+module Poltergeist
+
+using Base, ApproxFun, BandedMatrices, Compat, DualNumbers, DomainSets, StaticArrays, LightGraphs#, PyPlot#, FastTransforms
+using LinearAlgebra
+
+import Base: values,getindex,setindex!,*,.*,+,.+,-,.-,==,<,<=,>,|,
+  >=,./,/,.^,^,\,∪,∘,transpose, size, length,  eltype, inv, mod, convert#issymmetric,
+@compat import LinearAlgebra: eigvals, eigvecs
+import ApproxFun: domainspace, rangespace, domain, israggedbelow, RaggedMatrix,
+  resizedata!, colstop, CachedOperator, Infinity,
+  fromcanonicalD, tocanonicalD, fromcanonical, tocanonical, space, eigs, cfstype, prectype
+import DomainSets: AbstractInterval, UnionDomain
+
+export (..), Interval, PeriodicSegment, Fun, eigs
+
+include("general.jl")
+include("maps/maps.jl")
+include("HGraphs.jl")
+include("hofbauerextension.jl")
+include("InducedMap.jl")
+include("BackSum.jl")
+include("spectral/spectral.jl")
+include("timeseries.jl")
+include("poetry.jl")
+
+end # module
